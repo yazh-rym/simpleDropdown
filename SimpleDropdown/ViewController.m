beba8fdb7f52b07019035649075dcf857b3a0232
329 Mac <329mac@329s-mac-mini.local>
2016-02-16 11:50:14 +0530

    Added Simple dropdown with Button and Table

--- SimpleDropdown/ViewController.m
+++ SimpleDropdown/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSArray *arrayTableData;
+    BOOL isDropdownOpen;
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *tableviewDropdown;
 
 @end
 
@@ -16,7 +22,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    isDropdownOpen = 0;
+    
+    arrayTableData = @[@"Siva", @"Kumar", @"Cwah", @"Cumaar"];
+    
+    _tableviewDropdown.hidden = YES;
+    
+    _tableviewDropdown.delegate = self;
+    _tableviewDropdown.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +38,68 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showDropdown:(id)sender {
+    
+    if (isDropdownOpen) {
+        _tableviewDropdown.hidden = NO;
+        
+        [UIView animateWithDuration:2.0f
+                              delay:1.5f
+                            options: UIViewAnimationOptionTransitionFlipFromTop
+                         animations:^{
+                             _tableviewDropdown.hidden = YES;
+                         }
+                         completion:^(BOOL finished){
+                             NSLog(@"Done!");
+                         }];
+        isDropdownOpen = 0;
+        
+
+    } else {
+        _tableviewDropdown.hidden = YES;
+        
+        [UIView animateWithDuration:2.0f
+                              delay:1.5f
+                            options: UIViewAnimationOptionTransitionFlipFromTop
+                         animations:^{
+                             _tableviewDropdown.hidden = NO;
+                         }
+                         completion:^(BOOL finished){
+                             NSLog(@"Done!");
+                         }];
+        isDropdownOpen = 1;
+
+    }
+    
+
+    
+}
+
+#pragma mark - Tableview delegate method
+#pragma mark -
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [arrayTableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                       reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [arrayTableData objectAtIndex:indexPath.row];
+    
+    return cell;
+}
 @end
